65ca449a7638092a56f218ddf87ad36689c5882f
nfttoken <84901128+nfttoken@users.noreply.github.com>
2022-02-11 10:42:23 +0800

    Add files via upload

---
+++ precache-manifest.f1476cbff3952b456cce4dcd0ae4c08f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa4265820ffc4c99e59494748c85609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dabdcd22d0d900f366d9",
+    "url": "/static/css/main.14686620.chunk.css"
+  },
+  {
+    "revision": "39b4ad0490a826b78387",
+    "url": "/static/js/2.fa80e35b.chunk.js"
+  },
+  {
+    "revision": "679dc0b0298984b03ab372d214526e9a",
+    "url": "/static/js/2.fa80e35b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dabdcd22d0d900f366d9",
+    "url": "/static/js/main.d8c4d3e5.chunk.js"
+  },
+  {
+    "revision": "ae2dfafbfc2365c4ffff",
+    "url": "/static/js/runtime-main.f7c5443e.js"
+  }
+]);
